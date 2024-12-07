d594b5ba27c8196cee0b746b7271b2b5615ba936
Nikki <bogdnico@gmail.com>
2024-12-07 15:49:55 -0600

    stati

--- config/settings.py
+++ config/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath("static"))]
+STATIC_ROOT = str(BASE_DIR.joinpath("staticfiles"))
 STORAGES = {
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
